8800d2e4ef34e9d543cc97b6a5724f083ec91b31
James Hannah <jhannah@protonmail.com>
2022-01-04 16:13:09 -0800

    format find-duplicates

--- find-duplicates.py
+++ find-duplicates.py
@@ -9,9 +9,17 @@ def main():
     ids = {}
     shell_exts = ["bot", "che", "cdt", "ubc", "med", "xbt"]
     to_exclude = ["HISTORY", "old", "Archive", "Bottle", "BOTTLE"]
-    print("Files included in", ", ".join(to_exclude), "subdirectories were not compared for duplicates")
+    print(
+        "Files included in",
+        ", ".join(to_exclude),
+        "subdirectories were not compared for duplicates",
+    )
     # make a list of all elements in shell_exts followed by their str.upper() versions
-    exts = [item for sublist in [[ext, ext.upper()] for ext in shell_exts] for item in sublist]
+    exts = [
+        item
+        for sublist in [[ext, ext.upper()] for ext in shell_exts]
+        for item in sublist
+    ]
     for root, dirs, files in os.walk("data"):
         for ext in exts:
             for item in fnmatch.filter(files, f"*.{ext}"):
@@ -21,10 +29,12 @@ def main():
                 except Exception as e:
                     logging.exception(f"Failed to read {file_name}: {e}")
                     continue
-                id = shell.file.start_time.strftime("%Y/%m/%dT%H:%M:%S") +\
-                    shell.administration.mission +\
-                    shell.location.station +\
-                    str(shell.location.event_number)
+                id = (
+                    shell.file.start_time.strftime("%Y/%m/%dT%H:%M:%S")
+                    + shell.administration.mission
+                    + shell.location.station
+                    + str(shell.location.event_number)
+                )
                 if id in ids:
                     print(file_name, "may be a duplicate of", ids[id])
                 ids[id] = file_name
